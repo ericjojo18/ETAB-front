379229ad4468385362240bb6d3945f118127c48a
N'dri eric <ndrieric@192.168.1.34>
2024-08-22 12:57:55 +0000

    front

--- asset/js/menu.js
+++ asset/js/menu.js
@@ -2,7 +2,7 @@
 
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
-const closeBtn = document.querySelector("#close-btn");
+ const closeBtn = document.querySelector("#close-btn");
 const themeToggler = document.querySelector(".theme-toggler");
 
 menuBtn.addEventListener('click', ()=>{
@@ -10,8 +10,8 @@ menuBtn.addEventListener('click', ()=>{
 })
 
 closeBtn.addEventListener('click', ()=>{
-    sideMenu.style.display = 'none';
-})
+     sideMenu.style.display = 'none';
+ })
 
 themeToggler.addEventListener('click', ()=>{
     document.body.classList.toggle('dark-theme-variables');
